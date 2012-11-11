2f48732b49959d7444cffec00ad2b93ac0cd0ea0
Frantisek Havluj <moskyt@rozhled.cz>
2012-11-11 19:20:31 +0100

    tweaking deployrb

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require "bundler/capistrano"
+load 'deploy/assets'
+
+set :application, "urboretum"
+
+set :scm, :git
+set :repository, "https://github.com/moskyt/urboretum-server.git"
+set :branch, "master"
+
+# set :repository_cache, "git_cache"
+# set :deploy_via, :remote_cache
+#set :git_shallow_clone, 1
+set :ssh_options, { :forward_agent => true }
+set :user, 'moskyt'
+set :use_sudo, false
+
+role :app, "siven.onesim.net"
+
+role :web, "siven.onesim.net"
+role :db,  "siven.onesim.net", :primary => true
+set :deploy_to, "/home/moskyt/urboretum"
+
+task :events do
+  run "cd #{current_path}; bundle exec rake events:build RAILS_ENV=production"
+end
