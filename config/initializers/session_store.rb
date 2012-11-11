0fb413d3ea8ff625b63960c29c1b6383e0287dd5
Moskyt <moskyt@siven.onesim.net>
2012-11-11 19:38:27 +0100

    r18

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-UrboretumServer::Application.config.session_store :cookie_store, key: '_urboretum-server_session'
+UrboretumServer::Application.config.session_store :cookie_store, :key => '_urboretum-server_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
