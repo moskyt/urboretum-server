3c882213d23eae52f59c14279cfdddab797f0c9b
Frantisek Havluj <moskyt@rozhled.cz>
2012-11-12 10:08:46 +0100

    fixed production callback url

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -6,7 +6,7 @@ unless Rails.env.development?
     config.client_secret = "5ada964d373d4c269bba0298ce3a65ee"
   end
 else
-  CALLBACK_URL = "http://localhost:3000/oauth/callback"
+  CALLBACK_URL = "http://urboretum.zemezamyslena.cz/oauth/callback"
 
   Instagram.configure do |config|
     config.client_id = "0e29b96d8cad49df81893a24e7ce8f7d"
