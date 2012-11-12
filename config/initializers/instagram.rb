610f0c718236838c44a10b17caf3de521b6304a1
Moskyt <moskyt@siven.onesim.net>
2012-11-12 10:12:09 +0100

    typo fix

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,12 +1,12 @@
 unless Rails.env.development?
-  CALLBACK_URL = "http://localhost:3000/oauth/callback"
+  CALLBACK_URL = "http://urboretum.zemezamyslena.cz/oauth/callback"
 
   Instagram.configure do |config|
     config.client_id = "6bc2e659607f4de8b6ec4b6cf0c467ec"
     config.client_secret = "5ada964d373d4c269bba0298ce3a65ee"
   end
 else
-  CALLBACK_URL = "http://urboretum.zemezamyslena.cz/oauth/callback"
+  CALLBACK_URL = "http://localhost:3000/oauth/callback"
 
   Instagram.configure do |config|
     config.client_id = "0e29b96d8cad49df81893a24e7ce8f7d"
